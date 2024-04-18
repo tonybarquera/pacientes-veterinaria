a325e2a8cd0be65038ad2383fba2a7e34b763e12
Tony Barquera <barqueratony@gmail.com>
2024-04-18 09:22:45 -0600

    [ADD] patient info and buttons

--- src/components/PatientDetails.tsx
+++ src/components/PatientDetails.tsx
@@ -1,4 +1,5 @@
 import { Patient } from "../types";
+import PatientDetailItem from "./PatientDetailItem";
 
 type PatientDetailsProps = {
   patient: Patient
@@ -6,8 +7,18 @@ type PatientDetailsProps = {
 
 function PatientDetails({ patient } : PatientDetailsProps) {
   return (
-    <div>
-      {patient.name}
+    <div className="mx-5 my-10 px-5 py-10 bg-white shadow-md rounded-md">
+      <PatientDetailItem label="ID" data={patient.id} />
+      <PatientDetailItem label="Nombre" data={patient.name} />
+      <PatientDetailItem label="Propietario" data={patient.caretaker} />
+      <PatientDetailItem label="Email" data={patient.email} />
+      <PatientDetailItem label="Fecha alta" data={patient.date.toString()} />
+      <PatientDetailItem label="Síntomas" data={patient.symptoms} />
+
+      <div className="flex justify-between mt-10">
+        <button className="py-2 px-8 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg">Editar</button>
+        <button className="py-2 px-8 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg">Eliminar</button>
+      </div>
     </div>
   )
 }

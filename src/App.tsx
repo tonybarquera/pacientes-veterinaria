572909ccfebff10463714e4ed577f7235168f67b
Tony Barquera <barqueratony@gmail.com>
2024-04-18 10:21:14 -0600

    deploy github pages

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import 'react-toastify/dist/ReactToastify.css'
 function App() {
   return (
     <>
-      <div className="container mx-auto mt-20 px-2">
+      <div className="container mx-auto mt-10 px-2">
         <h1 className="font-black text-5xl text-center md:w-2/3 md:mx-auto">Seguimiento de Pacientes {' '}
           <span className="text-indigo-700">Veterinaria</span>
         </h1>

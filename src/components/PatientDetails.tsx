8bec23d72f8943768be46858c2668c1c81e680c8
Tony Barquera <barqueratony@gmail.com>
2024-04-18 10:07:07 -0600

    [ADD] delete notification

--- src/components/PatientDetails.tsx
+++ src/components/PatientDetails.tsx
@@ -1,3 +1,4 @@
+import { toast } from "react-toastify";
 import { usePatientStore } from "../store/store";
 import { Patient } from "../types";
 import PatientDetailItem from "./PatientDetailItem";
@@ -8,6 +9,14 @@ type PatientDetailsProps = {
 
 function PatientDetails({ patient } : PatientDetailsProps) {
   const { deletePatient, getPatientById } = usePatientStore();
+
+  const handleClick = (id: Patient['id']) => {
+    deletePatient(id);
+    toast('Paciente Eliminado', {
+      type: 'error'
+    })
+  }
+
   return (
     <div className="mx-5 my-10 px-5 py-10 bg-white shadow-md rounded-md">
       <PatientDetailItem label="ID" data={patient.id} />
@@ -19,7 +28,7 @@ function PatientDetails({ patient } : PatientDetailsProps) {
 
       <div className="flex flex-col md:flex-row justify-between mt-10 gap-3">
         <button className="py-2 px-8 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg" onClick={() => getPatientById(patient.id)}>Editar</button>
-        <button className="py-2 px-8 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg" onClick={() => deletePatient(patient.id)}>Eliminar</button>
+        <button className="py-2 px-8 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg" onClick={() => handleClick(patient.id)}>Eliminar</button>
       </div>
     </div>
   )
